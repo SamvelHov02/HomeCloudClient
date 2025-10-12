65c171fcfcf0d3c5a38c3b334b107e001edc9aa1
Samvel Hovhannisyan <samnar2002@icloud.com>
2025-10-12 14:16:03 +0200

    Update File Implemented #1
    The UpdateFile() implemented as mentioned in #1. The function can
    update existing files and if the files doesn't exist it will create
    and write to it.

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	Node "client/node"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("Hello from the Client side")
+	method := 1
+	resp := Node.Start(method, "test1.md")
+
+	/*
+		For which operations does the Client need to change ITS local Vault
+		GET 	: YES, needs to update since it's used for syncing
+		POST	: NO, tells server to sync with the changes client has made
+		PUT 	: NO, tells server to sync with the changes client has made
+		DELETE 	: NO, tells server to sync with the changes client has made
+	*/
+	if method == 1 {
+		Node.UpdateFile(resp, "test1.md")
+	}
 }
